2b94cf5b1593498fafc2926b1a1bfe3ba6b84356
KABU09 <eduardoayalesleon@gmail.com>
2021-04-03 22:37:47 -0600

    toggle up-arrow

---
+++ Observatorio/src/app/components/fab/fab.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-fab',
+  templateUrl: './fab.component.html',
+  styleUrls: ['./fab.component.css']
+})
+export class FabComponent implements OnInit {
+  @Input() color: string;
+  @Input() backgroundColor: string;
+  
+  constructor() {
+    // backgroundColor="var(--fab-background-color)" color="var(--fab-color)"
+    // this.color = '#FF0000';
+    // this.backgroundColor = '#CFCFCF'; 
+    this.color = 'var(--fab-color)';
+    this.backgroundColor = 'var(--fab-background-color)';    
+  }
+
+  ngOnInit(): void {
+  }
+
+}
+
