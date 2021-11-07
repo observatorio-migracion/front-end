dfdae1967566ba7d11c6c5c51fefc96b6bfeea70
JuanG05 <juangarro05@gmail.com>
2021-11-06 23:23:04 -0600

    https://www.notion.so/Reuni-n-6-11-2021-Cambios-al-footer-y-cambios-tentativos-a-los-estilos-b8095cebfc7e461fb2afb86fe758fb5d Avances en el footer

--- Observatorio/src/app/components/footer/footer.component.ts
+++ Observatorio/src/app/components/footer/footer.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/app/models/Post';
+import { PostService } from 'src/app/services/post.service';
+import { ThemeService } from '../../services/theme.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +10,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  public matchQuery: any;
+  public theme: string;
+  public subscription:Subscription | undefined;
 
-  constructor() { }
+  constructor(private _themeService:ThemeService) {
+    this.theme = 'light';
+    this.matchQuery = window.matchMedia('(max-width: 767px)').matches;
+   }
 
-  ngOnInit(): void {
+   ngOnInit(): void {
+    this.subscription = this._themeService.currentTheme.subscribe(theme => {
+      this.theme = theme;
+    })
   }
 
 }
